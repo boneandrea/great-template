f4542d0cdb367bd1e8679bba0fb05eccb44e832d
boneandrea <bone.andrea.anthrax@gmail.com>
2022-10-18 18:49:59 +0900

    Fix tool

--- tool/setup.sh
+++ tool/setup.sh
@@ -7,11 +7,11 @@ cd `dirname $0`/..
 # export MAIL_PORT=8026 # if change
 
 docker-compose build --no-cache
+docker-compose up -d
 docker-compose run --rm web composer install --no-interaction
 docker-compose run --rm web bin/cake migrations migrate -p CakeDC/Users
 docker-compose run --rm web bin/cake migrations migrate
 docker-compose run --rm web bin/cake migrations seed --seed UsersSeed
 docker-compose run --rm web bin/cake cache clear _cake_core_
-docker-compose up -d
 
 # docker-compose run --rm web composer test
