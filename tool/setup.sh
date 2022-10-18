f528ac0100f966827516fdcdc45db9d1f11df0ff
boneandrea <bone.andrea.anthrax@gmail.com>
2022-10-18 17:50:04 +0900

    Add tool

---
+++ tool/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd `dirname $0`/..
+
+# export WEB_PORT=3007 # if change
+# export DB_PORT=3307 # if change
+# export MAIL_PORT=8026 # if change
+
+docker-compose build --no-cache
+docker-compose run --rm web composer install --no-interaction
+docker-compose run --rm web bin/cake migrations migrate -p CakeDC/Users
+docker-compose run --rm web bin/cake migrations migrate
+docker-compose run --rm web bin/cake migrations seed --seed UsersSeed
+docker-compose run --rm web bin/cake cache clear _cake_core_
+docker-compose up -d
+
+# docker-compose run --rm web composer test
