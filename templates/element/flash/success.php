67cfdc215854c0187e8ac2b8563d1de8a5c40f31
boneandrea <bone.andrea.anthrax@gmail.com>
2023-02-17 10:20:36 +0900

    Fix style of flash message

--- templates/element/flash/success.php
+++ templates/element/flash/success.php
@@ -8,7 +8,7 @@ if (!isset($params['escape']) || $params['escape'] !== false) {
     $message = h($message);
 }
 ?>
-<div class="message success alert alert-success alert-dismissible fade show" role="alert" onclick="this.classList.add('hidden')">
+<div class="alert alert-success" role="alert" onclick="this.classList.add('d-none')">
     <?= $message ?>
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
 	<span aria-hidden="true">&times;</span>
